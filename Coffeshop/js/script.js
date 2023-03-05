2ad0241fd91e4ddee508adeb7e8586985e678d34
KevinZakky <kevinzakky9@gmail.com>
2023-03-05 11:58:41 +0700

    Menambahkan project ke dalam folder

---
+++ Coffeshop/js/script.js
@@ -0,0 +1,10 @@
+// Toggle class active
+
+const navbarNav = document.querySelector
+('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').
+onclick = () =>{
+    navbarNav.classList.toggle('active');
+}
---
+++ ResponsiveWeb/script.js
@@ -0,0 +1,15 @@
+var menubtn = document.getElementById("menubtn");
+var sidenav = document.getElementById("sidenav");
+
+sidenav.style.right = "-250px";
+menubtn.onclick = function(){
+    if(sidenav.style.right == "-250px"){
+        sidenav.style.right = "0";
+    }else{
+        sidenav.style.right = "-250px";
+    }
+    var scroll = new SmoothScroll('a[href*="#"]',{
+        speed:1000,
+        speedAsDuration:true
+    });
+}
